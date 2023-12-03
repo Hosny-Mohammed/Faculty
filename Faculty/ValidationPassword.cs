ce40f90f8be5c98c22d705f9e57f5f348210d6cc
Hosny-Mohammed <hosnymohammed2007@gmail.com>
2023-12-03 09:17:02 +0200

    Change in password func

--- Faculty/ValidationPassword.cs
+++ Faculty/ValidationPassword.cs
@@ -10,8 +10,8 @@ namespace Faculty
     {
         public  bool IsValid(String pass)
         {
-            bool upper, lower, numeric, sympole;
-            sympole = upper = lower = numeric = false;
+            bool upper, lower, numeric, sympol;
+            sympol = upper = lower = numeric = false;
             string SpecailChars = @"[!@#$%^&*()_+=\[{\]};:<>|./?,-]";
             foreach (char c in pass)
             {
@@ -32,11 +32,11 @@ namespace Faculty
                 }
                 else if (SpecailChars.Contains(c))
                 {
-                    sympole = true;
+                    sympol = true;
                     // return true;
                 }
             }
-            return upper && lower && numeric && sympole;
+            return upper && lower && numeric && sympol;
         }
     }
 }
